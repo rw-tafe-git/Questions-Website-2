d7f449dd7a7073b27e2ca1c45e0cde1331214f35
rw-tafe-git <124329775+rw-tafe-git@users.noreply.github.com>
2023-10-31 13:28:11 +0800

    Updated for final

--- config.php
+++ config.php
@@ -2,19 +2,29 @@
 
 function connect() {
 	
-	$host = 'localhost';
-	$dbname = 'questionsdb';
-	$username = 'root'; 
-	$password = ''; 
-	
-	if (strpos(dirname(__FILE__), "var/www/")) {
+	 // Database configuration  
+    $dbHost     = "localhost";
+    $dbName     = "questionsdb";
+    // xampp's Username & Password
+    $dbUsername = "root";
+    $dbPassword = "";
+
+    // Webmin's Username & Password (if() checks current file path).
+    if (strpos(dirname(__FILE__), "var/www/")) {
       $dbUsername = "adminer";
       $dbPassword = "P@ssw0rd";
     }
-	
-	try {
-		return new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-	} catch (PDOException $e) {
-		die("Error: " . $e->getMessage());
-	}
+
+    try {
+      $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $conn;
+    } catch (PDOException $ex) {
+      $GLOBALS["validConn"] = false;
+      $conn = null;
+      displayAlert("A connection error has occured: <br> The required database couldn't be found on the server.
+        <br> System Message: " . $ex->getMessage(), "warning");
+    } finally {
+      return $conn;
+    }
 }
